9ed8eb68c271f0a432196f50d3357dc403a29ed1
shijun <shijun1985911@163.com>
2016-04-20 22:37:11 +0800

    FLASK day2 0.2

--- app/views.py
+++ app/views.py
@@ -9,7 +9,9 @@ from flask import render_template
 @app.route('/index')
 def index():
     title = u"shijun"
-    post = {"username": "shijun", "usersex": 30, "commint": u"我是你爸爸爸爸"}
+    post = {"username": "shijun", "usersex": 30, "commint": u"我是你爸爸爸爸"},\
+           {"username": "shijie", "usersex": 31,"commint": u"我是妈妈妈妈"}, \
+           {"username": "chenxiaolu","usersex": 55,"commint": u"我是你滴滴滴滴滴"}
     return render_template('index.html',
                            post=post,
                            title=title)
