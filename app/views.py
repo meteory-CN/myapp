f93aa09daa0111057ef191df9134afcb2e953144
shijun <shijun1985911@163.com>
2016-04-21 21:54:34 +0800

    0.4

--- app/views.py
+++ app/views.py
@@ -2,16 +2,22 @@
 # coding=utf-8
 
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .form import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     title = u"shijun"
-    post = {"username": "shijun", "usersex": 30, "commint": u"我是你爸爸爸爸"},\
-           {"username": "shijie", "usersex": 31,"commint": u"我是妈妈妈妈"}, \
-           {"username": "chenxiaolu","usersex": 55,"commint": u"我是你滴滴滴滴滴"}
+    post = {"username": "shijun", "usersex": 30, "commint": u"我是你爸爸爸爸"}, \
+           {"username": "shijie", "usersex": 31, "commint": u"我是妈妈妈妈"}, \
+           {"username": "chenxiaolu", "usersex": 55, "commint": u"我是你滴滴滴滴滴"}
     return render_template('index.html',
                            post=post,
                            title=title)
+
+
+@app.route('/login')
+def login():
+    form = Loginform()
